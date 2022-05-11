0bc6f30974d599ede0966227b97cf979c493de04
kitri <kitri@DESKTOP-4G1D7HS>
2022-05-11 11:13:41 +0900

    member local 커밋

--- Kistagram/src/main/java/kr/co/kitri/member/service/MemberSvc.java
+++ Kistagram/src/main/java/kr/co/kitri/member/service/MemberSvc.java
@@ -1,11 +1,14 @@
 package kr.co.kitri.member.service;
 
+import org.springframework.stereotype.Service;
+
 import kr.co.kitri.member.vo.MemberVO;
 
+@Service
 public interface MemberSvc {
-//	public boolean idCheck(String id);
+	public boolean idCheck(String id);
 	public boolean signIn(MemberVO mvo);
-//	public boolean signUp(MemberVO mvo);
+	public boolean signUp(MemberVO mvo);
 
 
 }
